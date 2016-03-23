3aa5269df8a1c12e617667351993b7da40927b06
dannyschot1992 <dannyshot1992@gmail.com>
2016-03-23 21:10:37 +0100

    Administratie page (knoppen)

--- includes/overzicht.inc.php
+++ includes/overzicht.inc.php
@@ -1,5 +1,4 @@
-<?php
-echo '
+
 <div class="row">
         <div class="col-lg-12">
             <h1>Overzicht</h1>
@@ -19,5 +18,5 @@ Van datum <input type="date" name="Van-Datum" id="Van-Datum1" tabindex="1" class
         <div class="form-group">
 Tot datum <input type="date" name="Tot-Datum" id="Tot-Datum1" tabindex="2" class="form-control" placeholder="Tot" value="">
         </div>
-
-    </div> '; ?>
+        <button type="submit" form="form1" value="Submit">Submit</button>
+    </div>
