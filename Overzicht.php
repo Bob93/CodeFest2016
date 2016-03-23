adfd5042f58e6c237814ec91fd94b607b93a196e
Bob93 <goossen.bob@gmail.com>
2016-03-23 21:21:07 +0100

    bij inloggen 2 functies toegevoegd voor het hashen van wachtwoorden

--- Overzicht.php
+++ Overzicht.php
@@ -5,9 +5,7 @@ if (isset($_SESSION['page'])){ // als er een pagina gedefinieerd is
 } else {
     $pagina = 'overzicht';	// De default pagina is landing
 }
-?>
-
-
+echo '
 <!DOCTYPE html>
 <html lang="en">
 
@@ -28,7 +26,7 @@ if (isset($_SESSION['page'])){ // als er een pagina gedefinieerd is
     <link href="css/logo-nav.css" rel="stylesheet">
 
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!-- WARNING: Respond.js doesnt work if you view the page via file:// -->
     <!--[if lt IE 9]>
     <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
     <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
@@ -80,14 +78,16 @@ if (isset($_SESSION['page'])){ // als er een pagina gedefinieerd is
 
 <!-- /.container -->
 <div class="container">
-    <?php include 'includes/' . $pagina . '.inc.php';?>
-</div>
-<!-- jQuery -->
-<script src="js/jquery.js"></script>
+    '; include 'includes/' . $pagina . '.inc.php';
+echo '
+    </div>
+    <!-- jQuery -->
+    <script src="js/jquery.js"></script>
 
-<!-- Bootstrap Core JavaScript -->
-<script src="js/bootstrap.min.js"></script>
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
 
-</body>
+    </body>
 
-</html>
+    </html>
+';
