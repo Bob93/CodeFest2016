1bdaaf20e60abf2b2c4992c540fc9e4cb5ce0771
Bob93 <goossen.bob@gmail.com>
2016-03-23 22:55:05 +0100

    registratie formulier toegevoegd, danny kan hem gaan stylen. Nog niet getest of die werkt

---
+++ registratie.php
@@ -0,0 +1,120 @@
+<?php
+include 'connector.php';
+
+if(count($_POST)>0){
+    foreach($_POST as $key=>$values){
+        if(empty($_POST[$key])){
+                $error = ucfirst($key)." veld moet worden ingevuld!";
+                break;
+            }
+        }
+
+        // Validatie wachtwoord veld
+        if($_POST['wachtwoord']!=$_POST['wachtwoord_herhaling']){
+            $error= "Wachtwoord moet hetzelfde zijn!";
+        }
+
+        // Validatie voor checkbox
+        if(!isset($error) && !isset($_POST['geslacht'])){
+            $error = "Selecteer een geslact";
+        }
+
+        if(!isset($error)){
+            $voornaam = $_POST['voornaam'];
+            $tussenvoegsel = $_POST['tussenvoegsel'];
+            $achternaam = $_POST['achternaam'];
+            $gebruikersnaam = $_POST['gebruikersnaam'];
+            $wachtwoord = $_POST['wachtwoord'];
+            $geslacht = $_POST['geslacht'];
+            $geboortedatum = $_POST['geboortedatum'];
+            $sql = "INSERT INTO person
+            (voornaam,tussenvoegsel,achternaam,gebruikersnaam,wachtwoord,geslacht,geboortedatum)
+            VALUES (:voornaam,:tussenvoegsel,:achternaam,:gebruikersnaam,:wachtwoord,:geslacht,:geboortedatum)
+            ";
+            $query = $dbh->prepare($sql);
+            $query->execute(array(
+                ':voornaam'=>$voornaam,
+                ':tussenvoegsel'=>$tussenvoegsel,
+                ':achternaam'=>$achternaam,
+                ':gebruikersnaam'=>$gebruikersnaam,
+                ':wachtwoord'=>$wachtwoord,
+                ':geslacht'=>$geslacht,
+                ':geboortedatum'=>$geboortedatum
+        ));
+        if($query){
+            echo "Succesvol geregistreerd";
+        }
+    }
+}
+
+include 'includes/header.inc.php';
+?>
+    <body>
+        <form name="registratie" method="POST" action="">
+            <table border="0" cellspacing="15px">
+                <tr>
+                    <td colspan="2"><?php if(isset($error))echo $error; ?></td>
+                </tr>
+                <tr>
+                    <td>
+                        <h1>
+                            Registratie medewerker
+                        </h1>
+                    </td>
+                </tr>
+                <tr>
+                    <td><p>Voornaam</p></td>
+                    <td>
+                        <input type="text" name="voornaam" class="inp_box" value="<?php if(isset($_POST['voornaam'])) echo $_POST['voornaam'];?>">
+                    </td>
+                </tr>
+                <tr>
+                    <td><p>Tussenvoegsel</p></td>
+                    <td>
+                        <input type="text" name="tussenvoegsel" class="inp_box" value="<?php if(isset($_POST['tussenvoegsel'])) echo $_POST['tussenvoegsel'];?>">
+                    </td>
+                </tr>
+                <tr>
+                    <td><p>Achternaam</p></td>
+                    <td>
+                        <input type="text" name="achternaam" class="inp_box" value="<?php if(isset($_POST['achternaam'])) echo $_POST['achternaam'];?>">
+                    </td>
+                </tr>
+                <tr>
+                    <td><p>Gebruikersnaam</p></td>
+                    <td>
+                        <input type="gebruikersnaam"  name="" value="<?php if(isset($_POST['gebruikersnaam'])) echo $_POST['gebruikersnaam'];?>" class="inp_box">
+                </tr>
+                <tr>
+                    <td><p>Wachtwoord herhalen</p></td>
+                    <td>
+                        <input type="password" name="wachtwoord_herhaling" value="<?php if(isset($_POST['wachtwoord_herhaling'])) echo $_POST['wachtwoord_herhaling'];?>" class="inp_box">
+                    </td>
+                </tr>
+                <tr>
+                    <td><p>Geboortedatum</p></td>
+                    <td>
+                        <input type="geboortedatum" name="email" class="inp_box" value="<?php if(isset($_POST['geboortedatum'])) echo $_POST['geboortedatum'];?>">
+                    </td>
+                </tr>
+                <tr>
+                    <td><p>Geslacht</p></td>
+                    <td>
+                        <input type="radio" name="geslacht" value="M"
+                               <?php if(isset($_POST['geslacht']) && $_POST['geslacht']=='M'):?>checked<?php endif ?> >M
+                        <input type="radio" name="geslacht" value="F"
+                               <?php if(isset($_POST['geslacht']) && $_POST['geslacht']=='F'):?>checked<?php endif ?>>F
+                    </td>
+                </tr>
+                <tr>
+                    <td colspan="2">
+                        <input type="submit" value="registreren" name="submit" class="sub_btn">
+                    </td>
+
+                </tr>
+            </table>
+
+        </form>
+    </body>
+</html>
+
