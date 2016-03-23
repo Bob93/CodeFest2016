02b2268a0ed8da200c4779ed84bf0f8e22c93955
Bob93 <goossen.bob@gmail.com>
2016-03-23 20:52:52 +0100

    updated some files

--- Vakantie.php
+++ Vakantie.php
@@ -1,30 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="">
-
-    <title>Logo Nav - Start Bootstrap Template</title>
-
-    <!-- Bootstrap Core CSS -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-    <link href="css/bootstrap.css" rel="stylesheet">
-    <!-- Custom CSS -->
-    <link href="css/logo-nav.css" rel="stylesheet">
-
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-    <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-    <![endif]-->
-
-</head>
+<?php
+include 'connector.php';
+include 'includes/header.inc.php';
+?>
 
 <body>
 
@@ -47,10 +24,10 @@
     <div class="menu-info">
     <div class="panel-body">
         <div class="form-group">
-            Van datum <input type="date" name="Van-Datum" id="Van-Datum" tabindex="1" class="form-control" placeholder="Van" value="">
+            Van datum <input type="date" name="van-datum" id="van-datum" tabindex="1" class="form-control" placeholder="Van" value="">
         </div>
         <div class="form-group">
-            Tot datum <input type="date" name="Tot-Datum" id="Tot-Datum" tabindex="2" class="form-control" placeholder="Tot" value="">
+            Tot datum <input type="date" name="tot-datum" id="tot-datum" tabindex="2" class="form-control" placeholder="Tot" value="">
         </div>
         <input type="submit" value="Submit">
     </div>
