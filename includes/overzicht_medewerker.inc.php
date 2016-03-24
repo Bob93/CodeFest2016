989b764ff054dac4b5cacf2ed4f360a4003f6f84
dannyschot1992 <dannyshot1992@gmail.com>
2016-03-24 03:31:08 +0100

    Chart gemaakt( Test) kan ikzelf niet

--- includes/overzicht_medewerker.inc.php
+++ includes/overzicht_medewerker.inc.php
@@ -1,1 +1,15 @@
 <?php
+
+
+$result = mysql_query("SELECT verlof, uren, overuren  FROM ziekte en verlof, uren WHERE verlof=:verlof, uren=:uren, overuren=:overuren");
+
+$rows = array();
+while($r = mysql_fetch_array($result)) {
+    $row[0] = $r[0];
+    $row[1] = $r[1];
+    array_push($rows,$row);
+}
+
+print json_encode($rows, JSON_NUMERIC_CHECK);
+
+?>
--- js/bootstrap.js
+++ js/bootstrap.js
@@ -1,3 +1,47 @@
+
+$(document).ready(function() {
+    var options = {
+        chart: {
+            renderTo: 'container',
+            plotBackgroundColor: null,
+            plotBorderWidth: null,
+            plotShadow: false
+        },
+        title: {
+            text: 'Web Sales & Marketing Efforts'
+        },
+        tooltip: {
+            formatter: function() {
+                return '<b>'+ this.point.name +'</b>: '+ this.percentage +' %';
+            }
+        },
+        plotOptions: {
+            pie: {
+                allowPointSelect: true,
+                cursor: 'pointer',
+                dataLabels: {
+                    enabled: true,
+                    color: '#000000',
+                    connectorColor: '#000000',
+                    formatter: function() {
+                        return '<b>'+ this.point.name +'</b>: '+ this.percentage +' %';
+                    }
+                }
+            }
+        },
+        series: [{
+            type: 'pie',
+            name: 'Browser share',
+            data: []
+        }]
+    }
+
+    $.getJSON("data.php", function(json) {
+        options.series[0].data = json;
+        chart = new Highcharts.Chart(options);
+    });
+
+});
 $(function() {
 
     $('#login-form-link').click(function(e) {
